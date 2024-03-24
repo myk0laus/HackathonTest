3928f9205799aacebc47302bd0f9f0de51522e67
n1clasz <levchuk036@gmail.com>
2024-03-24 22:14:49 +0200

    Add border checking

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -4,13 +4,28 @@ using UnityEngine;
 public class CollisionHandler : MonoBehaviour
 {
     [SerializeField] private Score _score;
+    [SerializeField] private LayerMask _borderMask;
+    [SerializeField] private float _checkingDistance;
+    [SerializeField] private float _maxCheckingTime;
+    private float _currentCheckingTime;
+
     public event Action Dead;
 
+    private void Start()
+    {
+        _currentCheckingTime = _maxCheckingTime;
+    }
+
+    private void Update()
+    {
+        BorderCheck();
+    }
+
     private void OnTriggerEnter(Collider other)
-    { 
+    {
         if (other.GetComponent<Obstacle>())
         {
-            _score.IncreaseScore();       
+            _score.IncreaseScore();
         }
     }
 
@@ -23,6 +38,22 @@ public class CollisionHandler : MonoBehaviour
         if (collision.collider.GetComponent<Obstacle>())
         {
             Dead?.Invoke();
+        }
+    }
+
+    private void BorderCheck()
+    {
+        if (Physics.Raycast(transform.position, transform.forward, _checkingDistance, _borderMask))
+        {
+            Debug.Log("I see border");
+            _currentCheckingTime -= Time.deltaTime;
+            if (_currentCheckingTime <= 0)
+                Dead?.Invoke();
+        }
+        else
+        {
+            _currentCheckingTime = _maxCheckingTime;
+            return;
         }
     }
 }
